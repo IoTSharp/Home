de38e03e86c2b0b442a77980ca5a5b31dd687a6d
qian wang <1510106069@qq.com>
2023-01-28 17:40:20 +0800

    fix: 贡献者部分调整 fix: footer部分调整

--- pages/components/contributors/index.tsx
+++ pages/components/contributors/index.tsx
@@ -1,6 +1,9 @@
 import {FC} from "react";
-import {Col, Row} from '@douyinfe/semi-ui';
+import {Row, Col, Descriptions} from '@douyinfe/semi-ui';
 import styles from "./styles.module.scss";
+import Image from "next/image";
+import ContributorForQQ from '@/public/contributor_1.png';
+import ContributorForWechat from '@/public/contributor_2.png';
 
 export interface IContributorsProps {
 }
@@ -8,18 +11,20 @@ export interface IContributorsProps {
 const Contributors: FC<IContributorsProps> = ({}) => {
   return (
     <div className={styles.contributorsContainer}>
-      <Row className={styles.row} type="flex" align="middle">
-        <Col span={7}>
-          <div className={styles.title}>参与建设</div>
-          <div className={styles.description}>很多小伙伴在和我们一同建设 IoTSharp，如果你有兴趣，欢迎加入我们 。</div>
+      <p className={styles.title}>与用户共同成长</p>
+      <p className={styles.subTitle}>IoTSharp 重视我们的用户，加入并助力我们不断完善</p>
+      <Descriptions align="center" size="large" row>
+        <Descriptions.Item itemKey="Stars">795+</Descriptions.Item>
+        <Descriptions.Item itemKey="Fork">1100+</Descriptions.Item>
+        <Descriptions.Item itemKey="Download">1000+</Descriptions.Item>
+        <Descriptions.Item itemKey="Contributors">30+</Descriptions.Item>
+      </Descriptions>
+      <Row gutter={80}>
+        <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+          <Image src={ContributorForQQ} alt="" width={90} height={90}/>
         </Col>
-        <Col span={10} offset={2}>
-          <a href="https://github.com/iotsharp/iotsharp/graphs/contributors">
-            <img src="https://contrib.rocks/image?repo=iotsharp/iotsharp" />
-          </a>
-          <a href="https://github.com/iotsharp/ClientAppWithVue3/graphs/contributors">
-            <img src="https://contrib.rocks/image?repo=iotsharp/ClientAppWithVue3" />
-          </a>
+        <Col xs={24} sm={24} md={12} lg={12} xl={12}>
+          <Image src={ContributorForWechat} alt="" width={90} height={90}/>
         </Col>
       </Row>
     </div>
