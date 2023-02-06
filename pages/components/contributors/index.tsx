c1681bbc4d9635f7d3acbedd6c01c0b0a25b0ea8
OweQian <1510106069@qq.com>
2023-02-06 21:45:51 +0800

    feat: 贡献者模块

--- pages/components/contributors/index.tsx
+++ pages/components/contributors/index.tsx
@@ -1,27 +1,189 @@
 import {FC} from "react";
-import {Col, Row} from '@douyinfe/semi-ui';
+import {Row, Col, Descriptions, Card, Avatar, Space, Button, Typography} from '@douyinfe/semi-ui';
 import styles from "./styles.module.scss";
 
 export interface IContributorsProps {
 }
 
 const Contributors: FC<IContributorsProps> = ({}) => {
+  const { Meta } = Card;
+  const { Text } = Typography;
   return (
     <div className={styles.contributorsContainer}>
-      <Row className={styles.row} type="flex" align="middle">
-        <Col span={7}>
-          <div className={styles.title}>参与建设</div>
-          <div className={styles.description}>很多小伙伴在和我们一同建设 IoTSharp，如果你有兴趣，欢迎加入我们 。</div>
-        </Col>
-        <Col span={10} offset={2}>
-          <a href="https://github.com/iotsharp/iotsharp/graphs/contributors">
-            <img src="https://contrib.rocks/image?repo=iotsharp/iotsharp" />
-          </a>
-          <a href="https://github.com/iotsharp/ClientAppWithVue3/graphs/contributors">
-            <img src="https://contrib.rocks/image?repo=iotsharp/ClientAppWithVue3" />
-          </a>
-        </Col>
-      </Row>
+      <p className={styles.title}>与用户共同成长</p>
+      <p className={styles.subTitle}>IoTSharp 重视我们的用户，加入并助力我们不断完善</p>
+      <Descriptions align="center" size="large" row>
+        <Descriptions.Item itemKey="Stars">795+</Descriptions.Item>
+        <Descriptions.Item itemKey="Fork">1100+</Descriptions.Item>
+        <Descriptions.Item itemKey="Download">1000+</Descriptions.Item>
+        <Descriptions.Item itemKey="Contributors">30+</Descriptions.Item>
+      </Descriptions>
+      <div className={styles.imageBox}>
+        <Row gutter={32} type="flex" align="top">
+          <Col span={8}>
+            <Card
+              footerLine={false}
+              headerLine={false}
+              title={
+                <Meta
+                  title=""
+                  description=""
+                  avatar={
+                    <Avatar
+                      alt='Card meta img'
+                      size="default"
+                      src='https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/card-meta-avatar-docs-demo.jpg'
+                    />
+                  }
+                />
+              }
+              footerStyle={{ display: 'flex', justifyContent: 'flex-end' }}
+              footer={
+                <Space>
+                  <div>@Mengzhou</div>
+                </Space>
+              }
+            >
+              接入简单易上手；API 丰富全面。
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              footerLine={false}
+              headerLine={false}
+              title={
+                <Meta
+                  title=""
+                  description=""
+                  avatar={
+                    <Avatar
+                      alt='Card meta img'
+                      size="default"
+                      src='https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/card-meta-avatar-docs-demo.jpg'
+                    />
+                  }
+                />
+              }
+              footerStyle={{ display: 'flex', justifyContent: 'flex-end' }}
+              footer={
+                <Space>
+                  <div>@Mengzhou</div>
+                </Space>
+              }
+            >
+              有很多公司内的其他业务使用，有比较多的样例参考，参考其他业务平台的设计方案，统一平台的交付标准。
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              footerLine={false}
+              headerLine={false}
+              title={
+                <Meta
+                  title=""
+                  description=""
+                  avatar={
+                    <Avatar
+                      alt='Card meta img'
+                      size="default"
+                      src='https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/card-meta-avatar-docs-demo.jpg'
+                    />
+                  }
+                />
+              }
+              footerStyle={{ display: 'flex', justifyContent: 'flex-end' }}
+              footer={
+                <Space>
+                  <div>@Mengzhou</div>
+                </Space>
+              }
+            >
+              接入简单易上手；API 丰富全面。
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              footerLine={false}
+              headerLine={false}
+              title={
+                <Meta
+                  title=""
+                  description=""
+                  avatar={
+                    <Avatar
+                      alt='Card meta img'
+                      size="default"
+                      src='https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/card-meta-avatar-docs-demo.jpg'
+                    />
+                  }
+                />
+              }
+              footerStyle={{ display: 'flex', justifyContent: 'flex-end' }}
+              footer={
+                <Space>
+                  <div>@Mengzhou</div>
+                </Space>
+              }
+            >
+              接入简单易上手；API 丰富全面。
+            </Card>
+          </Col>
+          <Col span={8} style={{marginTop: '40px'}}>
+            <Card
+              footerLine={false}
+              headerLine={false}
+              title={
+                <Meta
+                  title=""
+                  description=""
+                  avatar={
+                    <Avatar
+                      alt='Card meta img'
+                      size="default"
+                      src='https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/card-meta-avatar-docs-demo.jpg'
+                    />
+                  }
+                />
+              }
+              footerStyle={{ display: 'flex', justifyContent: 'flex-end' }}
+              footer={
+                <Space>
+                  <div>@Mengzhou</div>
+                </Space>
+              }
+            >
+              接入简单易上手。
+            </Card>
+          </Col>
+          <Col span={8}>
+            <Card
+              footerLine={false}
+              headerLine={false}
+              title={
+                <Meta
+                  title=""
+                  description=""
+                  avatar={
+                    <Avatar
+                      alt='Card meta img'
+                      size="default"
+                      src='https://lf3-static.bytednsdoc.com/obj/eden-cn/ptlz_zlp/ljhwZthlaukjlkulzlp/card-meta-avatar-docs-demo.jpg'
+                    />
+                  }
+                />
+              }
+              footerStyle={{ display: 'flex', justifyContent: 'flex-end' }}
+              footer={
+                <Space>
+                  <div>@Mengzhou</div>
+                </Space>
+              }
+            >
+              接入简单易上手；API 丰富全面。
+            </Card>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
